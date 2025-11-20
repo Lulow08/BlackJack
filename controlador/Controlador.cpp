a2efb10c138f0e4a6b685c22187803f15192428f
Lulow08 <yostinomgdan08@gmail.com>
2025-11-19 22:32:59 -0500

    feature: añadir metodo obtenereleccionmenu

--- controlador/Controlador.cpp
+++ controlador/Controlador.cpp
@@ -1,9 +1,32 @@
 #include "Controlador.h"
 
+#include <iostream>
+
 Controlador::Controlador(Vista& pVista) : vista{pVista} {}
 
 void Controlador::obtenerEleccionMenu() const {
-    // Logica para obtener la elección del menu
+    int opcion = 0;
+
+    vista.mostrarMenu();
+    vista.mostrarTexto("Seleccione una opción: ");
+
+    switch (opcion) {
+        case 1:
+            // Empezar nueva partida
+            break;
+        case 2:
+            vista.mostrarTexto("VERSIÓN DEMO, FUNCIONALIDAD NO IMPLEMENTADA");
+            break;
+        case 3:
+            vista.mostrarTexto("VERSIÓN DEMO, FUNCIONALIDAD NO IMPLEMENTADA");
+            break;
+        case 4:
+            vista.mostrarTexto("VERSIÓN DEMO, FUNCIONALIDAD NO IMPLEMENTADA");
+            break;
+        case 5:
+            // Salir del juego
+            break;
+    }
 }
 
 void Controlador::obtenerAccionJugador() const {
