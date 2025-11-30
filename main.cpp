d5825632e7ee22c37268217ce1714e1edbf953e4
Lulow08 <yostinomgdan08@gmail.com>
2025-11-30 16:40:51 -0500

    refactor: mejora de menu apuesta

--- main.cpp
+++ main.cpp
@@ -5,23 +5,44 @@
 #include "Jugador.h"
 #include "Mazo.h"
 #include "Controlador.h"
+#include "Apuesta.h"
 #include <iostream>
 
 int main() {
     Vista vista;
     Mazo mazo;
+    Apuesta apuesta;
+
     Crupier crupier(mazo, vista);
     Jugador jugador(vista);
-    Controlador controlador(vista);
-
-    vista.mostrarPantallaPrincipal();
+    Controlador controlador(vista, jugador, apuesta);
 
     while (true) {
-        int opcion = controlador.getOpcionMenuPrincipal(1, 5);
+        vista.mostrarPantallaPrincipal();
+        int opcion = controlador.getOpcionInt(1, 5);
 
         switch (opcion) {
-            case 1:
-                break;
+            case 1: {
+                vista.limpiarPantalla();
+                vista.mostrarTitulo();
+                std::string nombre = controlador.getNombreJugador(3, 8);
+                jugador.setNombre(nombre);
+
+                vista.mostrarPantallaApuesta(nombre, apuesta.getDineroTotal(), apuesta.getApuestaActual());
+                char opcionApuesta = controlador.getOpcionChar("WQCVB", APUESTA);
+                switch (opcionApuesta) {
+                    case 'W':
+                        crupier.empezarNuevaRonda(jugador);
+                        vista.mostrarPantallaJuego();
+                        std::cin.get();
+                        break;
+                    
+                    default:
+                        break;
+                }
+            }
+            break;
+
             case 2:
                 break;
             case 3:
--- modelos/Jugador.cpp
+++ modelos/Jugador.cpp
@@ -1,4 +1,8 @@
 #include "Jugador.h"
 #include <iostream>
 
-Jugador::Jugador(Vista& pVista, const std::string& pNombre) : Participante(pVista), nombre(pNombre) {}
+Jugador::Jugador(Vista& pVista) : Participante(pVista) {}
+
+void Jugador::setNombre(const std::string& pNombre) {
+    nombre = pNombre;
+}
